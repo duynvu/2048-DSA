fe576453f0d39b6e6c86a2daf42d53e60524f0fe
quangloc <locpq0311@gmail.com>
2019-05-04 21:22:44 +0700

    draw table

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,1 +1,20 @@
-alert("hello");
+let grid;
+
+function blankGrid() {
+    return [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+}
+
+function setup() {
+    createCanvas(400, 400);
+    noLoop();
+    //console.table(grid);
+    grid = blankGrid();
+    // addNumber();
+    // addNumber();
+    updateCanvas();
+}
