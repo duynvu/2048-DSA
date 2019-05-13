77c8fd10db2d16bf5867d0351b36098560a0f824
quangloc <locpq0311@gmail.com>
2019-05-13 22:46:16 +0700

    interact with grid

---
+++ public/javascript/grid.js
@@ -0,0 +1,22 @@
+let grid;
+
+function blankGrid() {
+    return [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ];
+}
+
+//compare each number
+function compare(a, b) {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (a[i][j] !== b[i][j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
