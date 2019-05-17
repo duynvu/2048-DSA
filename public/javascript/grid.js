2ee2737a8a5c7f03ef7ce6919a7ccfcfd2287df9
quangloc <locpq0311@gmail.com>
2019-05-17 22:44:52 +0700

    add number

--- public/javascript/grid.js
+++ public/javascript/grid.js
@@ -9,7 +9,26 @@ function blankGrid() {
     ];
 }
 
-//compare each number
+function addNumber() {
+    let options = [];
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (grid[i][j] === 0) {
+                options.push({
+                    x: i,
+                    y: j
+                });
+            }
+        }
+    }
+    if (options.length > 0){
+        let spot = random(options);
+        let r = random(1);
+        grid[spot.x][spot.y] = r > 0.5 ? 2 : 4;
+    }
+}
+
+//compare each number in row
 function compare(a, b) {
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
