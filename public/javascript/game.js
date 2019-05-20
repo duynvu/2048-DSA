bcab27d474a62e1b84d98072b96a54baa0c716bf
Trung Doan <dttrung.ityu@gmail.com>
2019-05-20 09:34:30 +0700

    check lose

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -8,3 +8,22 @@ function isGameWon() {
     } 
     return false;   
 }
+
+function isGameOver() {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (grid[i][j] == 0) {
+                return false;
+            }
+
+            if (i !== 3 && grid[i][j] === grid[i + 1][j]) {
+                return false;
+            }
+
+            if (j !== 3 && grid[i][j] === grid[i][j + 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
